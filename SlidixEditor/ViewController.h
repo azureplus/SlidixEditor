f400011d661dbacd4e42a361e3bd2b21468074f8
MiniCnKruz <cnKruz@users.noreply.github.com>
2015-06-24 11:56:22 -0500

    Initial Commit

---
+++ SlidixEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlidixEditor
+//
+//  Created by Eduardo on 6/24/15.
+//  Copyright (c) 2015 Eduardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
